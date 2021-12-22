6b65ff5d44df4cc0dcc10bd057fcea24d7aeb73b
TheLegend <jinhyuk558@gmail.com>
2021-12-21 18:24:17 -0800

    read stream data from lichess and serve to caller

---
+++ index.js
@@ -0,0 +1,52 @@
+require('dotenv').config()
+const express = require('express')
+const fetch = require('node-fetch')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const readStream = require('./nodejs-ndjson-stream-reader')
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+
+// example: http://localhost:5000/api/games?max=10&prefType=blitz&username=DrNykterstein
+app.get('/api/games', (req, res) => {
+  // console.log(req.query.max)
+  // console.log(req.query.prefType)
+  // console.log(req.query.username)
+  const max = req.query.max 
+  const prefType = req.query.prefType 
+  const username = req.query.username
+  
+  let games = []
+  const headers = {
+    'Accept': 'application/x-ndjson',
+    'Authorization': 'Bearer ' + process.env.lichessToken,
+  }
+  const url = `https://lichess.org/api/games/user/${username}?max=${max}&perfType=${prefType}`
+  console.log(url)
+  const stream = fetch(url, { headers })
+  const onMessage = obj => {
+    games.push(obj)
+  }
+  const onComplete = () => {
+    console.log('The stream has completed. Below is the list of games')
+    console.log(games)
+    res.json(games)
+  }
+  stream
+    .then(
+      readStream(onMessage)
+    )
+    .catch(() => {
+      return res.status(500).send('Some error')
+    })
+    .then(onComplete)
+    
+})
+
+app.listen(5000, () => console.log('app listening on 5000 ...'))
+
+// ?max=a&perfType=b&username=c
---
+++ nodejs-ndjson-stream-reader.js
@@ -0,0 +1,32 @@
+// CREDIT: ornicar (https://gist.github.com/ornicar/a097406810939cf7be1df8ea30e94f3e)
+
+/* FOR NODEJS
+Utility function to read a ND-JSON HTTP stream.
+`processLine` is a function taking a JSON object. It will be called with each element of the stream.
+`response` is the result of a `fetch` request.
+See usage example in the next file.
+*/
+const readStream = processLine => response => {
+  const matcher = /\r?\n/;
+  const decoder = new TextDecoder();
+  let buf = '';
+  return new Promise((resolve, fail) => {
+    response.body.on('data', v => {
+      const chunk = decoder.decode(v, { stream: true });
+      buf += chunk;
+
+      const parts = buf.split(matcher);
+      buf = parts.pop();
+      for (const i of parts.filter(p => p)) {
+        processLine(JSON.parse(i));
+      }
+    });
+    response.body.on('end', () => {
+      if (buf.length > 0) processLine(JSON.parse(buf));
+      resolve();
+    });
+    response.body.on('error', fail);
+  });
+};
+
+module.exports = readStream
